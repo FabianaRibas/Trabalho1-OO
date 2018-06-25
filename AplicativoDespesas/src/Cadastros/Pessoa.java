35996a46b13e6e53839798e49de6d15292a25581
FabianaRibas <fabianalv.ribas@gmail.com>
2018-06-25 12:29:33 -0300

    class Cadastro

--- AplicativoDespesas/src/Cadastros/Pessoa.java
+++ AplicativoDespesas/src/Cadastros/Pessoa.java
@@ -5,12 +5,14 @@ public class Pessoa {
 	String nome;
 	String email;
 	float rendimento;
+	float totalPagar;
 	
-	Pessoa(String nome, String email, float rendimento)
+	Pessoa(String nome, String email, float rendimento, float totalPagar)
 	{
 		this.nome = nome;
 		this.email = email;
 		this.rendimento = rendimento;
+		this.totalPagar = totalPagar;
 	}
 
 	public String getNome() {
@@ -25,12 +27,18 @@ public class Pessoa {
 		return rendimento;
 	}
 	
+	
+	public float getTotalPagar() {
+		return totalPagar;
+	}
+
 	public String toString()
 	{
 		String info = "";
 		info += "Nome: " + nome + '\n';
 		info += "Email: " + email + '\n';
 		info += "Rendimento: " + rendimento + '\n';
+		info += "Total a Pagar: " + totalPagar + '\n';
 		return info;
 		
 	}
